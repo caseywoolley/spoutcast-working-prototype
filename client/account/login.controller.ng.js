c5c0c78613986370b32dafb23647c86e08724cfb
Casey Woolley <caseywoolley@gmail.com>
2016-03-09 12:05:03 -0600

    added side menu login form

--- client/account/login.controller.ng.js
+++ client/account/login.controller.ng.js
@@ -1,6 +1,19 @@
 'use strict'
 
 angular.module('spoutCastApp')
-.controller('LoginCtrl', function($scope) {
-  $scope.viewName = 'Login';
+.controller('LoginCtrl', function($scope, $meteor) {
+  
+	$scope.login = function() {
+		console.log($scope.email, $scope.password)
+		$meteor.loginWithPassword( $scope.email, $scope.password ).then(function(){
+		  console.log('Login success');
+		}, function(err){
+		  console.log('Login error - ', err);
+		});
+	};
+
+	$scope.logout = function() {
+		$meteor.logout();
+	};
+
 });
