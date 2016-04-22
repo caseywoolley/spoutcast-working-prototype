155b27710d68136deee97c674ee4325066767cd3
Casey Woolley <caseywoolley@gmail.com>
2016-04-22 07:54:20 -0500

    added splash images and config for ios testflight

--- mobile-config.js
+++ mobile-config.js
@@ -1,37 +1,51 @@
-// This section sets up some basic app metadata,
-// the entire section is optional.
-// App.info({
-//   id: 'com.caseywoolley.spoutcast',
-//   name: 'spoutcast',
-//   description: 'Get über power in one button click',
-//   author: 'Matt Development Group',
-//   email: 'contact@example.com',
-//   website: 'http://example.com'
-// });
+App.info({
+  id: 'com.caseywoolley.spoutcast',
+  name: 'spoutcast',
+  description: 'Reviews you can trust and watch',
+  author: 'Casey Woolley',
+  email: '',
+  website: ''
+});
 
-// // Set up resources such as icons and launch screens.
-// App.icons({
-//   'iphone': 'icons/icon-60.png',
-//   'iphone_2x': 'icons/icon-60@2x.png',
-//   // ... more screen sizes and platforms ...
-// });
+//generated using https://github.com/lpender/meteor-assets
+App.icons({
+	"iphone": "resources/icons/iphone.png", // 60x60
+  "iphone_2x": "resources/icons/iphone_2x.png", // 120x120
+  "iphone_3x": "resources/icons/iphone_3x.png", // 180x180
+  "ipad": "resources/icons/ipad.png", // 76x76
+  "ipad_2x": "resources/icons/ipad_2x.png", // 152x152
+  // "ipad_pro": "resources/icons/ipad_pro.png", // 167x167
+  // "ios_settings": "resources/icons/ios_settings.png", // 29x29
+  // "ios_settings_2x": "resources/icons/ios_settings_2x.png", // 58x58
+  // "ios_settings_3x": "resources/icons/ios_settings_3x.png", // 87x87
+  // "ios_spotlight": "resources/icons/ios_spotlight.png", // 40x40
+  // "ios_spotlight_2x": "resources/icons/ios_spotlight_2x.png", // 80x80
+  // "android_mdpi": "resources/icons/android_mdpi.png", // 48x48
+  // "android_hdpi": "resources/icons/android_hdpi.png", // 72x72
+  // "android_xhdpi": "resources/icons/android_xhdpi.png", // 96x96
+  // "android_xxhdpi": "resources/icons/android_xxhdpi.png", // 144x144
+  // "android_xxxhdpi": "resources/icons/android_xxxhdpi.png" // 192x192
+});
 
-// App.launchScreens({
-//   'iphone': 'splash/Default~iphone.png',
-//   'iphone_2x': 'splash/Default@2x~iphone.png',
-//   // ... more screen sizes and platforms ...
-// });
-
-// // Set PhoneGap/Cordova preferences
-// App.setPreference('BackgroundColor', '0xff0000ff');
-// App.setPreference('HideKeyboardFormAccessoryBar', true);
-// App.setPreference('Orientation', 'default');
-// App.setPreference('Orientation', 'all', 'ios');
-
-// // Pass preferences for a particular PhoneGap/Cordova plugin
-// App.configurePlugin('com.phonegap.plugins.facebookconnect', {
-//   APP_ID: '1234567890',
-//   API_KEY: 'supersecretapikey'
-// });
+App.launchScreens({
+	"iphone": "resources/splashes/iphone.png", // 320x480
+  "iphone_2x": "resources/splashes/iphone_2x.png", // 640x490
+  "iphone5": "resources/splashes/iphone5.png", // 640x1136
+  "iphone6": "resources/splashes/iphone6.png", // 750x1334
+  "iphone6p_portrait": "resources/splashes/iphone6p_portrait.png", // 2208x1242
+  "iphone6p_landscape": "resources/splashes/iphone6p_landscape.png", // 2208x1242
+  "ipad_portrait": "resources/splashes/ipad_portrait.png", // 768x1024
+  "ipad_portrait_2x": "resources/splashes/ipad_portrait_2x.png", // 1536x2048
+  "ipad_landscape": "resources/splashes/ipad_landscape.png", // 1024x768
+  "ipad_landscape_2x": "resources/splashes/ipad_landscape_2x.png", // 2048x1536
+  // "android_mdpi_portrait": "resources/splashes/android_mdpi_portrait.png", // 320x480
+  // "android_mdpi_landscape": "resources/splashes/android_mdpi_landscape.png", // 480x320
+  // "android_hdpi_portrait": "resources/splashes/android_hdpi_portrait.png", // 480x800
+  // "android_hdpi_landscape": "resources/splashes/android_hdpi_landscape.png", // 800x480
+  // "android_xhdpi_portrait": "resources/splashes/android_xhdpi_portrait.png", // 720x1280
+  // "android_xhdpi_landscape": "resources/splashes/android_xhdpi_landscape.png", // 1280x720
+  // "android_xxhdpi_portrait": "resources/splashes/android_xxhdpi_portrait.png", // 1080x1440
+  // "android_xxhdpi_landscape": "resources/splashes/android_xxhdpi_landscape.png" // 1440x1080
+});
 
 App.accessRule('*');
